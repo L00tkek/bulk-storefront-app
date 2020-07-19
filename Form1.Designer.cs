2db25b8b39971c97d69f50a3cd2af5d612617427
L00tkek <46274859+L00tkek@users.noreply.github.com>
2020-07-18 18:24:36 -0700

    Source code and .exe for application

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace storefront
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numSelect = new System.Windows.Forms.NumericUpDown();
+            this.addButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkoutButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.checkOutput = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numSelect)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(0, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(161, 164);
+            this.listBox1.TabIndex = 1;
+            // 
+            // numSelect
+            // 
+            this.numSelect.Location = new System.Drawing.Point(3, 16);
+            this.numSelect.Name = "numSelect";
+            this.numSelect.Size = new System.Drawing.Size(78, 22);
+            this.numSelect.TabIndex = 2;
+            this.numSelect.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(87, 16);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(74, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkoutButton);
+            this.panel1.Controls.Add(this.removeButton);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.addButton);
+            this.panel1.Controls.Add(this.numSelect);
+            this.panel1.Location = new System.Drawing.Point(12, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(588, 254);
+            this.panel1.TabIndex = 4;
+            // 
+            // checkoutButton
+            // 
+            this.checkoutButton.Location = new System.Drawing.Point(273, 211);
+            this.checkoutButton.Name = "checkoutButton";
+            this.checkoutButton.Size = new System.Drawing.Size(118, 23);
+            this.checkoutButton.TabIndex = 9;
+            this.checkoutButton.Text = "Check Out";
+            this.checkoutButton.UseVisualStyleBackColor = true;
+            this.checkoutButton.Click += new System.EventHandler(this.checkoutButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(490, 16);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 8;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 214);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "$0.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total: ";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader1});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(167, 9);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(317, 199);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 1;
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 99;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 2;
+            this.columnHeader3.Text = "Number";
+            this.columnHeader3.Width = 65;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.DisplayIndex = 3;
+            this.columnHeader4.Text = "Subtotal";
+            this.columnHeader4.Width = 90;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.DisplayIndex = 0;
+            this.columnHeader1.Text = "Code";
+            // 
+            // checkOutput
+            // 
+            this.checkOutput.Location = new System.Drawing.Point(12, 269);
+            this.checkOutput.Name = "checkOutput";
+            this.checkOutput.ReadOnly = true;
+            this.checkOutput.Size = new System.Drawing.Size(588, 169);
+            this.checkOutput.TabIndex = 5;
+            this.checkOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 450);
+            this.Controls.Add(this.checkOutput);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numSelect)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numSelect;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button checkoutButton;
+        private System.Windows.Forms.RichTextBox checkOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace storefront
+{
+    public partial class Form1 : Form
+    {
+        //DollarAmount balance = new DollarAmount();
+        DollarAmount orderTotal = new DollarAmount();
+        Product[] products = //finite; array is useful
+            {
+                new Product(0, "placeholder 1", 10d),
+                new Product(1, "placeholder 2", 20d),
+                new Product(2, "placeholder 3", 30d)
+            }; 
+        List<ProductOrder> shoppingCart = new List<ProductOrder>(); //must be easy to change; thus, a list
+
+        //use panels to make the different pages
+
+        public Form1()
+        {
+            InitializeComponent();
+            //balanceLabel.Text = balance.ToString();
+
+            foreach(Product p in products)
+            {
+                listBox1.Items.Add(p);
+            }
+
+            listBox1.SelectedIndex = listBox1.TopIndex;
+
+            resizeColumns(listView1);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            Product currentProduct = (Product) listBox1.SelectedItem;
+            int amtToAdd = (int) numSelect.Value;
+            shoppingCart.Add(new ProductOrder(currentProduct, amtToAdd));
+
+            double subtotal = currentProduct.Price.Amt * amtToAdd;
+
+            ListViewItem order = new ListViewItem(currentProduct.Name); //this order is peculiar but necessary for the order to display properly
+            order.SubItems.Add("" + amtToAdd);
+            order.SubItems.Add(new DollarAmount(subtotal).ToString());
+            order.SubItems.Add("" + currentProduct.Code);
+            
+            listView1.Items.Add(order);
+
+            orderTotal.Amt += subtotal;
+            textBox1.Text = orderTotal.ToString();
+
+            resizeColumns(listView1);
+        }
+
+        private void resizeColumns(ListView listView1)
+        {
+            foreach (ColumnHeader column in listView1.Columns)
+            {
+                column.Width = -2;
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            for(int index = listView1.SelectedIndices.Count - 1; index >= 0; index--) //iterating backwards because iterating forwards can cause later indices to change when earlier items are removed
+            {
+                int i = listView1.SelectedIndices[index];
+
+                orderTotal.Amt -= shoppingCart.ElementAt(i).OrderProduct.Price.Amt * shoppingCart.ElementAt(i).OrderAmt;
+                textBox1.Text = orderTotal.ToString();
+
+                shoppingCart.RemoveAt(i);
+                listView1.Items.RemoveAt(i);
+            }
+        }
+
+        private void checkoutButton_Click(object sender, EventArgs e)
+        {
+            checkOutput.Text = "Product Order: \n\n";
+            foreach (ProductOrder p in shoppingCart)
+            {
+                checkOutput.Text += p.ToString() + "\n";
+            }
+
+            checkOutput.Text += $"\nTotal Cost: {textBox1.Text}";
+        }
+    }
+
+    class ProductOrder //wraps a product and a # of that product to order
+    {
+        public Product OrderProduct { get; private set; }
+        public int OrderAmt { get; private set; }
+
+        public ProductOrder(Product prod, int amt)
+        {
+            OrderProduct = prod;
+            OrderAmt = amt;
+        }
+
+        public override string ToString()
+        {
+            DollarAmount subtotal = new DollarAmount(OrderProduct.Price.Amt * OrderAmt);
+            return $"{OrderAmt} of Product {OrderProduct.Code} ({OrderProduct.Name}) for {subtotal}";
+        }
+    }
+
+    class Product
+    {
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+        public DollarAmount Price { get; private set; }
+
+        public Product(int code, string name, double price)
+        {
+            Code = code;
+            Name = name;
+            Price = new DollarAmount(price);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Price.ToString()}";
+        }
+
+        /*public string toListBox()
+        {
+            return $"{Name}: {Price.ToString()}";
+        } */
+        //product code, name, description, price
+    }
+
+    class DollarAmount
+    {
+        private double _amt;
+        public double Amt
+        {
+            get
+            {
+                return _amt;
+            }
+            set     //formats the input to two decimal places via truncating additional decimals
+            {       //important since .toString("C") rounds values instead, which is not a desired behavior
+                int temp = (int)(value * 100);
+                _amt = (double)temp / 100;
+            }
+        }
+
+        public DollarAmount()
+        {
+            _amt = 0;
+        }
+
+        public DollarAmount(double initVal)
+        {
+            Amt = initVal;
+        }
+
+        public override string ToString()
+        {
+            return _amt.ToString("C"); //formatted as a currency
+        }
+    }
+}
